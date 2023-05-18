5579ebedb996b2d157818f4b4f099d4fb419e43c
Josh Khalil <jk115720@ohio.edu>
2023-05-18 09:26:00 -0400

    Added source code

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Josh Khalil
+ *        Date: May 18, 2023
+ * Description: A simple program that outputs a message to the user.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
